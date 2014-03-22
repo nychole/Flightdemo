31772197c787ba4aa6e631956ac363438ff62240
nychole <nycholechang@gmail.com>
2014-03-23 00:31:07 +0100

    Flight Wrapper 1.0.3
    Add dynamic text wrapper
    Use Nokogiri instead of Watir

--- FlightGoogle.rb
+++ FlightGoogle.rb
@@ -2,6 +2,7 @@ class FlightGoogle
   require "date"
   require "watir-webdriver"
   require "csv"
+  require "watir-nokogiri"
 
   @@dtReserve = Date.today.to_s
   attr_accessor :obCity
@@ -19,99 +20,104 @@ class FlightGoogle
   end
   def gotob
     url = getURL
-    @b = Watir::Browser.new(:firefox)
-    @b.goto url
-    @b.driver.manage.timeouts.implicit_wait = 3 #3 seconds
-  end
-  def getData(id)
-    dataArr = []
-    d = @b.divs :class => id
-    d.each do |x|
-      dataArr << x.text
+    begin
+      @b = Watir::Browser.new(:firefox)
+      @b.goto url
+    rescue
+      @b.quit
+      retry
     end
+    @b.driver.manage.timeouts.implicit_wait = 2 #3 seconds
+    #puts @b.text
+    if @b.div(:text => "aller-retour").exist? then
+      @doc = Nokogiri::HTML(@b.html)
+    end
+  end
+
+  def getnbRow
+    return @doc.xpath("(//a/div/div[position()=1])").count
+  end
+  def getNoko(r)
+    dataArr = Hash.new
+=begin
+      doc.divs(:text => "aller-retour")[0].parent
+      a = a.parent until a.tag_name == 'a'
+      a.divs.map {|x| puts x.text }
+=end
+      dataArr[:dtPrice] = @doc.xpath("(//a/div[position()=1])[#{r}]").text.gsub(/[^\d]/,"")
+      dataArr[:dtComp] = @doc.xpath("(//a/div[position()=2])[#{r}]").text.gsub(/[^a-zA-Z ]/,'').gsub(/ +/,' ')
+      dataArr[:dtHours] = @doc.xpath("(//a/div[position()=3])[#{r}]").text.split(/[^\d]/).delete_if(&:empty?).join('h')
+      dataArr[:dtTrans] = @doc.xpath("(//a/div[position()=4])[#{r}]").text
+
+=begin
+      @doc.xpath('(//a/div[position()=4])').each do |x|
+      puts "-----"
+      #puts x.content
+      puts x.text #gsub(/[^a-zA-Z ]/,'').gsub(/ +/,' ') #gsub(/\d|\W/,"")
+        #puts a.xpath('following-sibling::text()').content
+        #end
+      #doc.css("a").each do |x|
+      #  puts x.content
+      end
+
+      doc.xpath("//a").map do |x|
+        puts "-----"
+        puts x.content
+      end
+=end
     return dataArr
   end
 
+  def getData(id)
+    dataArr = []
+    begin
+      d = @b.divs :class => id
+      d.each do |x|
+        dataArr << x.text
+      end
+    rescue
+      @b.quit
+      self.gotob
+      retry
+    end
+    return dataArr
+  end
   def getAllData #GJJKPX2CEC
     dataArr = Hash.new
-    #d = @b.divs :class => "gwt-HTML"
-    d = @b.elements :class => /^GJJKPX2CEC/
+    test =[]
+    dataArr['test']=[]
+    dataArr['test2']=[]
+    #d = @b.divs(:class => "gwt-HTML")[12]
+    #d = @b.elements :class => /^GJJKPX2CEC/
     r=0
-    d.each do |parent_level|
-      puts parent_level.div(:class => 'GJJKPX2JFC').text
-      #dataArr[r] = x.text
-      r+=1
-    end
+    #d = @b.div(:text => "aller-retour", :index=>0).parent.div.text
+    a = @b.div(:text => "aller-retour", :index=>0).parent
+    a = a.parent until a.tag_name == 'a'
+    #puts d
+    #dataArr = a.div.text # {|x| dataArr << x.text }
+    a.divs.map {|x| test << x.text }
+    #dataArr['test2']=a.div.text
+    dataArr['test1'] = test
+    puts "test1 ... #{dataArr['test1']}"
+    #dataArr.flatten! # you will get a one dimensional array
+    #dataArr.uniq! # removes all duplicate entries
+    test.delete_if {|i| i.include? "\n"} # use double quote instead of single quote
+    dataArr['test2'] = test
+    puts "test2 ... #{dataArr['test2']}"
     return dataArr
   end
 
   def getReport
     puts "this is the report as of #{@@dtReserve}..."
     @report = Hash.new
-    @report['dtPrice'] = getData('GJJKPX2JFC').collect { |x| x.gsub(/\D/,'').to_i }
-    nbRow = @report['dtPrice'].collect.size
-    #@report['ID'] = (1..nbRow).to_a
-    #puts " the id are #{@report['ID']}"
-    #puts " the prices are #{@report['dtPrice']}"
-    #puts "the price has size of #{nbRow}"
-    #@report['dtReserve'] = @@dtReserve #dupVect(@@dtReserve,nbRow)
-    #puts " the ReserveDates are #{@report['dtReserve']}"
-    #@report['obCity'] = obCity #dupVect(obCity,nbRow)
-    #@report['ibCity'] = ibCity#dupVect(ibCity,nbRow)
-    #@report['obDate'] = obDate#dupVect(obDate,nbRow)
-    #@report['ibDate'] = ibDate#dupVect(ibDate,nbRow)
-    @report['dtComp'] = getData('GJJKPX2PCC')[1..nbRow]
-    #puts " the company are #{@report['dtComp']}"
-    @report['dtHours'] = getData('GJJKPX2KDC')[1..nbRow]
-    @report['dtTrans'] = getData('GJJKPX2FGC')[1..nbRow]
-    #dtPrice.size.times{report << [@@dtReserve, obCity, ibCity, obDate, ibDate]}
-    #report << [dtComp[1..dtPrice.size], dtPrice, dtHours[1..dtPrice.size], dtTrans[1..dtPrice.size]]
-    #report = report.transpose
-    #puts "report done with #{@report.keys.length} of keys and #{@report.values.size} of values"
-    @Newflight = Hash.new
-    for i in 1..nbRow do
-      @Newflight['dtReserve'] = @@dtReserve
-      #@Newflight['obCity'] = obCity
-      #@Newflight['ibCity'] = ibCity
-      @Newflight['obDate'] = obDate
-      @Newflight['ibDate'] = ibDate
-      @Newflight["flights"]=Array(Hash.new)
-      @Newflight["flight-#{i}"]['dtComp'] = @report['dtComp'][i]
-      @Newflight["flight-#{i}"]['dtPrice'] = @report['dtPrice'][i]
-      @Newflight["flight-#{i}"]['dtHours'] = @report['dtHours'][i]
-      @Newflight["flight-#{i}"]['dtTrans'] = @report['dtTrans'][i]
-    end
-    return @Newflight
+    @report[:dtPrice] = getData('GJJKPX2JFC').collect { |x| x.gsub(/\D/,'').to_i }
+    nbRow = @report[:dtPrice].collect.size
+    @report[:dtComp] = getData('GJJKPX2PCC')[1..nbRow]
+    @report[:dtHours] = getData('GJJKPX2KDC')[1..nbRow]
+    @report[:dtTrans] = getData('GJJKPX2FGC')[1..nbRow]
+    return @report
   end
 
-=begin
-  def exportcsv
-    rowid = -1
-    CSV.open("testflight_#{@@dtReserve}.csv", 'w') do |csv|
-      $report.each do
-        rowid += 1
-        puts rowid
-        if rowid == 0
-          csv << $report.keys
-          # puts @report.keys
-        else
-            csv <<$report.values #.each do |x|
-              #csv << x
-              #end
-        end# of if/else inside hsh
-      end# of hsh's (rows)
-      end
-  end
-
-  def exportcsv
-    CSV.open("testflight_#{@@dtReserve}.csv", 'w') do |csv|
-      csv << @report.keys
-      @report.each do |key, val|
-        csv << [val].transpose
-      end
-    end
-  end
-=end
   def dupVect(arr,num)
     temp = []
     num.times {temp << arr.to_s}
